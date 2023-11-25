b367894d5eef5cecb338e9fc83f384b0d6d4fd9c
강상배 <sangbaekang@aboutfishing.kr>
2023-11-25 21:21:51 +0900

    initialize

---
+++ main.py
@@ -0,0 +1,20 @@
+import concurrent.futures
+import time
+
+def add_and_print(x):
+    time.sleep(1.234)
+
+    print(f'x print: {x}')
+    return x + 1
+
+def excute_thread():
+    # We can use a with statement to ensure threads are cleaned up promptly
+    with concurrent.futures.ThreadPoolExecutor(max_workers=5) as executor:
+        for i in range(10):
+            executor.submit(add_and_print, i)
+    
+    return "Success"
+
+
+if __name__ == "__main__":
+    print("Hello World")
